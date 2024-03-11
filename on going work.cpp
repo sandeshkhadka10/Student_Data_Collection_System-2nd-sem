3c61f217a79447bc7be351e57fc8d9ef9d0161fd
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-03-11 21:47:08 +0545

    solving the issue generated in the program

--- on going work.cpp
+++ on going work.cpp
@@ -42,29 +42,29 @@ void student::AddStudent()
 	   
 	   cout<<"\t\t\t\t\t<== Add Student Information ==>\n\n"<<endl;
 	
-	   cout<<"\n\t\tName: "<<;
+	   cout<<"\n\t\tName: ";
 	   cin.getline(obj.name,50);
 	
-    	cout<<"\n\t\tId no: "<<;
+    	cout<<"\n\t\tId no: ";
     	cin>>obj.id;
 	
-    	cout<<"\n\t\tCourse and Semester: "<<;
+    	cout<<"\n\t\tCourse and Semester: ";
     	cin.ignore();
     	cin.getline(obj.year,50);
 	
-    	cout<<"\n\t\tAddress: "<<;
+    	cout<<"\n\t\tAddress: ";
     	cin.getline(obj.address,50);
 	
-    	cout<<"\n\t\tEmail: "<<;
+    	cout<<"\n\t\tEmail: ";
     	cin.getline(obj.email,50);
 	
-    	cout<<"\n\t\tPhone no: "<<;
+    	cout<<"\n\t\tPhone no: ";
     	cin.getline(obj.phonenumber,50);
 	
-    	cout<<"\n\t\tGPA: "<<;
+    	cout<<"\n\t\tGPA: ";
     	cin>>obj.gpa;
 	
-    	cout<<"\n\t\tPaid Amount: "<<;
+    	cout<<"\n\t\tPaid Amount: ";
     	cin>>obj.paidAmount;
 	
     	obj.totalAmount=790000;//fixed amount
@@ -73,7 +73,7 @@ void student::AddStudent()
     	cout<<"\t\t__________________________________________"<<endl;
 	
     	ofstream fout;
-    	fout.open(filename,ios::binary | ios::app);
+    	fout.open("filename",ios::binary | ios::app);
     	if(!fout)
     	{
     		cout<<"\t\tUnable to open file.\n";
@@ -100,7 +100,7 @@ void student::DisplayAllStudent()
 	cout<<"\t\t\t\t\t<== Student Records ==>\n\n"<<endl;
 	
 	ifstream fin;
-	fin.open(filename,ios::binary);
+	fin.open("filename",ios::binary);
 	{
 		if(!fin)
 		{
@@ -140,11 +140,11 @@ void student::SearchStudent()
 	
 	cout<<"\t\t\t\t\t<== Search Student ==>\n\n"<<endl;
 	
-	cout<<"Enter the Id you want to search: "<<;
+	cout<<"Enter the Id you want to search: ";
 	cin>>search;
 	
 	ifstream fin;
-	fin.open(filename,ios::binary);
+	fin.open("filename",ios::binary);
 	if(!fin)
 	{
 		cout<<"\t\tUnable to open file.\n";
@@ -186,7 +186,7 @@ void student::EditStudentDetails()
 	student updated_student;
 	
 	ifstream fin;
-	fin.open(filename,ios::binary);
+	fin.open("filename",ios::binary);
 	if(!fin)
 	{
 		cout<<"\t\tUnable to open file.\n";
@@ -254,19 +254,19 @@ void student::EditStudentDetails()
 						
 					case 4:
 						cout<<"\n\t\tEnter Updated Address: "<<endl;
-						cin.getline(updated_student.address);
+						cin.getline(updated_student.address,50);
 						updated=1;
 						break;
 						
 					case 5:
 						cout<<"\n\t\tEnter Updated Email: "<<endl;
-						cin.getline(updated_student.email);
+						cin.getline(updated_student.email,50);
 						updated=1;
 						break;
 						
 					case 6:
 						cout<<"\n\t\tEnter Updated Phone number: "<<endl;
-						cin.getline(updated_student.phonenumber);
+						cin.getline(updated_student.phonenumber,50);
 						updated=1;
 						break;
 						
@@ -292,7 +292,7 @@ void student::EditStudentDetails()
 						{
 							cout<<"\n\t\tNo updates were made.\n"<<endl;					
 						}
-						repeatmenu=0;
+						repeatMenu=0;
 						break;
 						
 					default:
@@ -303,7 +303,7 @@ void student::EditStudentDetails()
 				}
 			}while(repeatMenu);
 			fstream finout;
-			finout.open(filename,ios::in || ios::out || ios::binary)
+			finout.open("students_records",ios::in | ios::out | ios::binary);
 			if(!finout)
 			{
 				cout<<"\t\tUnable to open file.\n";
@@ -324,7 +324,7 @@ void student::EditStudentDetails()
 }
 void student::DeleteStudent()
 {
-	int found=0;
+	int found=0,search;
 	
 	student obj;
 	
@@ -336,7 +336,7 @@ void student::DeleteStudent()
 	cin>>search;
 	
 	ifstream fin;
-	fin.open(filename,ios::binary);
+	fin.open("filename",ios::binary);
 	if(!fin)
 	{
 		cout<<"\t\tUnable to open file.\n";
@@ -348,23 +348,23 @@ void student::DeleteStudent()
 	{
 		cout<<"\t\tUnable to open file.\n";
 	}
-	fin.seekg(0,ios::beg)
-	{
-		if(obj.id==search)
-		{
-			found=1;
-			continue;
-		}
-		else
-		{
-			fout.write((char*)&obj,sizeof(obj));
-		}
-		
-	}
+	fin.seekg(0,ios::beg);
+    while (fin.read((char*)&obj, sizeof(obj)))
+    {
+       if (obj.id == search)
+       {
+         found = 1;
+         continue; // Skip writing obj to the output file
+       }
+       else
+       {
+          fout.write((char*)&obj, sizeof(obj));
+       }
+    }
 	fin.close();
 	fout.close();
-	remove(filename);
-	rename("temp_student_data",filename);
+	remove("filename");
+	rename("temp_student_data","filename");
 	if(found==1)
 	{
         cout<<"\t\tStudent with ID "<<search<<" deleted successfully.\n";
@@ -373,6 +373,118 @@ void student::DeleteStudent()
 	{
         cout<<"\t\tStudent with ID "<<search<<" not found.\n";
     }
+}
+int student::login() 
+{
+    string username, password;
+
+    cout << "\n\t\tUsername: ";
+    cin >> username;
+
+    cout << "\t\tPassword: ";
+    cin >> password;
+
+    if (username == "admin" && password == "12345") {
+        cout << "\n\t\tLogin successful\n";
+        cout << "\n\t\tLogging in . . . .\n";
+        sleep(3);
+        return 1; 
+    } else {
+        cout << "\n\t\tLogin failed! Invalid username or password.\n";
+        return 0; 
+    }
+}
+
+int main() {
+    char choice, x;
+    int loggedIn = 0; 
+
+    while (true) {
+        system("clear");
+        cout << "\t\t\t\t\t<== STUDENT MANAGEMENT SYSTEM ==>\n";
+        cout << "\t\t*********************************************************************************\n";
+
+        if (!loggedIn) {
+            cout << "\n\t\tLogin to continue:\n";
+            student student;
+            loggedIn = student.Login();
+            if (!loggedIn) {
+                cout << "\n\t\tPress any key to retry...\n";
+                cin.get();
+                continue;
+            }
+        }
+
+        while (true) {
+            system("clear");
+            cout << "\t\t\t\t\t<== Student Management System ==>\n";
+            cout << "\t\t*********************************************************************************\n";
+
+            cout << "\n\t\ta. Add Student\n";
+            cout << "\t\tb. Display All Students\n";
+            cout << "\t\tc. Search Student\n";
+            cout << "\t\td. Edit Student Details\n";
+            cout << "\t\te. Delete Student\n";
+            cout << "\t\tf. Exit\n";
+            cout << "\n\t\tEnter your choice(a-f): ";
+            cin >> choice; 
+            
+            switch (choice) {
+                case 'a': {
+                    Student student;
+                    student.AddStudent();
+                    break;
+                }
+                    
+                case 'b': {
+                    Student student;
+                    student.DisplayAllStudents();
+                    break;
+                }
+                    
+                case 'c': {
+                    Student student;
+                    student.SearchStudent();
+                    break;
+                }
+                    
+                case 'd': {
+                    Student student;
+                    student.EditStudentDetails();
+                    break;
+                }
+                    
+                case 'e': {
+                    Student student;
+                    student.DeleteStudent();
+                    break;
+                }
+                    
+                case 'f': {
+                    cout << "\t\tPress Y to exit and any other character to cancel: ";
+                    cin >> x;
+                    if (x == 'Y' || x == 'y') {
+                        exit(0);
+                    } else {
+                        break;
+                    }
+                }
+
+                default: {
+                    cout << "\n\t\tInvalid Choice!\n";
+                    cout << "\t\tPlease Enter from a to f\n";
+                    cout << "\t\tPress any key to retry";
+                    cin.get();
+                    break;
+                }
+            }
+            
+            cout << "\n\t\tPress any key to continue";
+            cin.get();
+        }
+    }
+    
+    return 0;
 }
 	
 	

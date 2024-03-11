8a1c644ecd94fa4a92fafe8648037c464eb4ff83
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-03-11 20:21:37 +0545

    edit fn completed

--- on going work.cpp
+++ on going work.cpp
@@ -184,86 +184,101 @@ void student::EditStudentDetails()
 	
 	student updated_student;
 	
-	fstream finout;
-	finout.open("studentdata",ios::in || ios::out | ios::binary);
-	finout.seekg(0,ios::beg);
-	while(finout.read((char*)&obj,sizeof(obj)))
+	ifstream fin;
+	fin.open(filename,ios::binary);
+	if(!fin)
+	{
+		cout<<"\t\tUnable to open file.\n";
+		return;
+	}
+	
+	student obj;
+	while(fin.read((char*)&obj,sizeof(obj)))
 	{
 		if(obj.id==id)
 		{
+			updated_student=obj;
 			found=1;
-			int choice;
-			int repeatMenu=1;
-			do
-			{
-				system("cls");
-				cout<<"\t\t\t\t\t<== Edit Student Details ==>\n\n"<<endl;
-				cout<<"\t\tSelect Fields to update: \n"<<endl;
-				cout<<"\t\t1.Name\n"<<endl;
-				cout<<"\t\t2.ID\n"<<endl;
-				cout<<"\t\t3.Semester\n"<<endl;
-				cout<<"\t\t4.Address\n"<<endl;
-				cout<<"\t\t5.Email\n"<<endl;
-				cout<<"\t\t6.Phone Number\n"<<endl;
-				cout<<"\t\t7.GPA\n"<<endl;
-				cout<<"\t\t8.Paid Amount\n"<<endl;
-				cout<<"\t\t9.Exit\n"<<endl;
-				cout<<"\t\tEnter your choice(1-9): "<<endl;
-				cin>>choice;
+			break;
+	    }
+	}
+	fin.close();
+	if(found==0)
+	{
+		cout<<"\n\t\tStudent with ID "<<id<<" not found.\n";
+		return;
+	}
+	
+	int choice;
+	int repeatMenu=1;
+	do
+	{
+		system("cls");
+		cout<<"\t\t\t\t\t<== Edit Student Details ==>\n\n"<<endl;
+		cout<<"\t\tSelect Fields to update: \n"<<endl;
+		cout<<"\t\t1.Name\n"<<endl;
+		cout<<"\t\t2.ID\n"<<endl;
+		cout<<"\t\t3.Semester\n"<<endl;
+		cout<<"\t\t4.Address\n"<<endl;
+		cout<<"\t\t5.Email\n"<<endl;
+		cout<<"\t\t6.Phone Number\n"<<endl;
+		cout<<"\t\t7.GPA\n"<<endl;
+		cout<<"\t\t8.Paid Amount\n"<<endl;
+	    cout<<"\t\t9.Exit\n"<<endl;
+	    
+		cout<<"\t\tEnter your choice(1-9): "<<endl;
+		cin>>choice;
+		cin.ignore();
+		
+		switch(choice)
+		{
+			case 1:
+				cout<<"\n\t\tEnter Updated Name: "<<endl;
 				fflush(stdin);
-				switch(choice)
-				{
-					case 1:
-						cout<<"\n\t\tEnter Updated Name: "<<endl;
-						fflush(stdin);
-						cin.getline(obj.name,sizeof(obj.name));
+						cin.getline(updated_student.name,50);
 						updated=1;
 						break;
 						
 					case 2:
 						cout<<"\n\t\tEnter Updated Id: "<<endl;
-						cin>>obj.id;
+						cin>>updated_student.id;
 						updated=1;
 						break;
 						
 					case 3:
 						cout<<"\n\t\tEnter Updated Semester: "<<endl;
-						fflush(stdin);
-						cin.getline(obj.year,sizeof(obj.year));
+						cin.getline(updated_student.year,50);
 						updated=1;
 						break;
 						
 					case 4:
 						cout<<"\n\t\tEnter Updated Address: "<<endl;
-						fflush(stdin);
-						cin.getline(obj.address,sizeof(obj.address));
+						cin.getline(updated_student.address);
 						updated=1;
 						break;
 						
 					case 5:
 						cout<<"\n\t\tEnter Updated Email: "<<endl;
-						fflush(stdin);
-						cin.getline(obj.email,sizeof(obj.email));
+						cin.getline(updated_student.email);
 						updated=1;
 						break;
 						
 					case 6:
 						cout<<"\n\t\tEnter Updated Phone number: "<<endl;
-						fflush(stdin);
-						cin.getline(obj.phonenumber,sizeof(obj.phonenumber));
+						cin.getline(updated_student.phonenumber);
 						updated=1;
 						break;
 						
 					case 7:
 						cout<<"\n\t\tEnter Updated GPA: "<<endl;
-						cin>>obj.gpa;
+						cin>>updated_student.gpa;
 						updated=1;
 						break;
 						
 					case 8:
 						cout<<"\n\t\tEnter Updated Payment Amount: "<<endl;
-						cin>>obj.paidAmount;
-						obj.dueAmount=obj.totalAmount-obj.paidAmount;
+						cin>>updated_student.paidAmount;
+						updated_student.dueAmount=updated_student.totalAmount-updated_student.paidAmount;
 						updated=1;
 						break;
 						
@@ -280,14 +295,37 @@ void student::EditStudentDetails()
 						break;
 						
 					default:
-						
+						cout<<"\n\t\tInvalid choice!";
+						cout<<"\n\t\tPress any key to retry";
+						cin.get();
+						break;
 				}
+			}while(repeatMenu);
+			fstream finout;
+			finout.open(filename,ios::in || ios::out || ios::binary)
+			if(!finout)
+			{
+				cout<<"\t\tUnable to open file.\n";
+				return;
 			}
-			
-		}
-	}
-	
+			finout.seekg(0,ios::beg);
+			while(finout.read((char*)&obj,sizeof(obj)))
+			{
+				if(obj.id==id)
+				{
+					int pos=finout.tellg();
+					finout.seekp(pos -sizeof(obj),ios::beg);
+					finout.write((char*)&obj,sizeof(obj));
+					break;
+				}
+	    	}
+	    	finout.close();
+}
+void student::DeleteStudent()
+{
 	
 }
+	
+	
 
 

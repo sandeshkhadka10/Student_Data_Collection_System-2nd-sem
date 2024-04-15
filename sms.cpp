9d955bac8bccb027def0c9e503e780c0cdb47523
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2024-04-15 18:09:39 +0545

    all documents added

--- sms.cpp
+++ sms.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
-#include<string.h>
-#include<string>
-#include<fstream>
-#include<conio.h>//for getch()
-#include<unistd.h>//for sleep()
-
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <fstream>
+#include <conio.h>  //for getch()
+#include <unistd.h> //for sleep()
+#define max_pass_len 10
+#define max_pin_len 4
 using namespace std;
 
 const string filename = "students_records.dat";
@@ -24,77 +25,40 @@ private:
     float dueAmount;
 
 public:
-    int Login();
     void AddStudent();
     void DisplayAllStudent();
     void SearchStudent();
     void EditStudentDetails();
     void DeleteStudent();
 };
-
-int student::Login()
-{
-    char username[50];
-    char password[50];
-
-    cout << "\n\t\tUsername: ";
-    cin >> username;
-
-    cout << "\t\tPassword: ";
-    cin >> password;
-
-    if (strcmp(username, "admin") == 0 && strcmp(password, "12345") == 0)
-    {
-        cout << "\n\t\tLogin successful\n";
-        cout << "\n\t\tLogging in . . . .\n";
-        sleep(3);
-        return 1;
-    }
-    else
-    {
-        cout << "\n\t\tLogin failed! Invalid username or password.\n";
-        return 0;
-    }
-}
-
 void student::AddStudent()
 {
     char another;
     do
     {
         student obj;
-
         system("cls");
-
-        cout << "\t\t\t\t\t<== Add Student Information ==>\n\n" << endl;
-
-        cout << "\n\t\tName: ";
         cin.ignore();
+        cout << "\t\t\t\t\t<== Add Student Information ==>\n\n"
+             << endl;
+        cout << "\n\t\tName: ";
         cin.getline(obj.name, 50);
-
         cout << "\n\t\tId no: ";
         cin >> obj.id;
-
-        cout << "\n\t\tCourse and Semester: ";
         cin.ignore();
+        cout << "\n\t\tCourse and Semester: ";
         cin.getline(obj.year, 50);
-
         cout << "\n\t\tAddress: ";
         cin.getline(obj.address, 50);
-
         cout << "\n\t\tEmail: ";
         cin.getline(obj.email, 50);
-
         cout << "\n\t\tPhone no: ";
         cin.getline(obj.phonenumber, 50);
-
         cout << "\n\t\tGPA: ";
         cin >> obj.gpa;
-
         cout << "\n\t\tPaid Amount: ";
         cin >> obj.paidAmount;
-
-        obj.totalAmount = 790000; //fixed amount
+        obj.totalAmount = 790000; // fixed amount
         obj.dueAmount = obj.totalAmount - obj.paidAmount;
 
         cout << "\t\t__________________________________________" << endl;
@@ -115,19 +79,16 @@ void student::AddStudent()
         cin.ignore();
 
     } while (another == 'y' || another == 'Y');
+    cout << "\n\t\tPress any key to go back....." << endl;
+    getch();
 }
-
 void student::DisplayAllStudent()
 {
     student obj;
-
     system("cls");
-
     int recordsfound = 0;
-
     cout << "\t\t\t\t\t<== Student Records ==>\n\n"
          << endl;
-
     ifstream fin;
     fin.open(filename.c_str(), ios::binary);
     if (!fin)
@@ -156,8 +117,9 @@ void student::DisplayAllStudent()
     {
         cout << "\n\t\tNo data found!!!!!" << endl;
     }
+    cout << "\n\t\tPress any key to go back....." << endl;
+    getch();
 }
-
 void student::SearchStudent()
 {
     student obj;
@@ -203,8 +165,9 @@ void student::SearchStudent()
         cout << "\n\t\tRecord not found\n"
              << endl;
     }
+    cout << "\n\t\tPress any key to go back....." << endl;
+    getch();
 }
-
 void student::EditStudentDetails()
 {
     system("cls");
@@ -212,7 +175,7 @@ void student::EditStudentDetails()
          << endl;
 
     int id = 0, found = 0, updated = 0;
-    cout << "\t\tEnter Id to edit: " << endl;
+    cout << "\t\tEnter Id to edit: ";
     cin >> id;
 
     student updated_student;
@@ -268,51 +231,51 @@ void student::EditStudentDetails()
         switch (choice)
         {
         case 1:
-            cout << "\n\t\tEnter Updated Name: " << endl;
-            cin.ignore();
+            // cin.ignore();
+            cout << "\n\t\tEnter Updated Name: ";
             cin.getline(updated_student.name, 50);
             updated = 1;
             break;
 
         case 2:
-            cout << "\n\t\tEnter Updated Id: " << endl;
+            cout << "\n\t\tEnter Updated Id: ";
             cin >> updated_student.id;
             updated = 1;
             break;
 
         case 3:
-            cout << "\n\t\tEnter Updated Semester: " << endl;
-            cin.ignore();
+            // cin.ignore();
+            cout << "\n\t\tEnter Updated Semester: ";
             cin.getline(updated_student.year, 50);
             updated = 1;
             break;
 
         case 4:
-            cout << "\n\t\tEnter Updated Address: " << endl;
+            cout << "\n\t\tEnter Updated Address: ";
             cin.getline(updated_student.address, 50);
             updated = 1;
             break;
 
         case 5:
-            cout << "\n\t\tEnter Updated Email: " << endl;
+            cout << "\n\t\tEnter Updated Email: ";
             cin.getline(updated_student.email, 50);
             updated = 1;
             break;
 
         case 6:
-            cout << "\n\t\tEnter Updated Phone number: " << endl;
+            cout << "\n\t\tEnter Updated Phone number: ";
             cin.getline(updated_student.phonenumber, 50);
             updated = 1;
             break;
 
         case 7:
-            cout << "\n\t\tEnter Updated GPA: " << endl;
+            cout << "\n\t\tEnter Updated GPA: ";
             cin >> updated_student.gpa;
             updated = 1;
             break;
 
         case 8:
-            cout << "\n\t\tEnter Updated Payment Amount: " << endl;
+            cout << "\n\t\tEnter Updated Payment Amount: ";
             cin >> updated_student.paidAmount;
             updated_student.dueAmount = updated_student.totalAmount - updated_student.paidAmount;
             updated = 1;
@@ -335,7 +298,7 @@ void student::EditStudentDetails()
         default:
             cout << "\n\t\tInvalid choice!";
             cout << "\n\t\tPress any key to retry";
-            cin.get();
+            // cin.get();
             break;
         }
     } while (repeatMenu);
@@ -348,9 +311,9 @@ void student::EditStudentDetails()
     finout.write((char *)&updated_student, sizeof(updated_student));
 
     finout.close();
+    cout << "\n\t\tPress any key to go back....." << endl;
+    getch();
 }
-
-
 void student::DeleteStudent()
 {
     int found = 0, search;
@@ -402,97 +365,308 @@ void student::DeleteStudent()
     {
         cout << "\t\tStudent with ID " << search << " not found.\n";
     }
+    cout << "\n\t\tPress any key to go back....." << endl;
+    getch();
+}
+
+class credentials
+{
+private:
+    char username[50];
+    char password[max_pass_len + 1];
+    char pin[max_pin_len + 1];
+    char p[max_pin_len + 1];
+    char temp_un[50];
+    char temp_pass[max_pass_len + 1];
+
+public:
+    void signup();
+    void pi_n();
+    int login();
+    void forgot_pass();
+};
+void credentials ::signup()
+{
+    credentials obj;
+    cout << "\n\t\tEnter new username: ";
+    cin >> obj.username;
+    int i = 0;
+    cout << "\t\tEnter new password(10 characters): ";
+    while (true)
+    {
+        char ch = _getch();
+        if (ch == 13) // enter key ascii value=13
+        {
+            break;
+        }
+        else if (ch == 8) // Backspace key ascii value = 8
+        {
+            if (i > 0)
+            {
+                i--;
+                cout << "\b \b"; // Move the cursor back and erase the character
+            }
+        }
+        else if (ch == 9 || ch == 32)
+        {
+            continue;
+        }
+        else if (i < max_pass_len)
+        {
+            obj.password[i] = ch;
+            cout << '*';
+            i++;
+        }
+    }
+    obj.password[i] = '\0';
+    ofstream fout;
+    fout.open("p_confidentials", ios::trunc | ios::binary);
+    fout.write((char *)&obj, sizeof(obj));
+    fout.close();
+}
+void credentials ::pi_n()
+{
+    credentials obj;
+    int j = 0;
+    cout << "\n\t\tEnter pin(used while doing forget password/4 character): ";
+    while (true)
+    {
+        char c = _getch();
+        if (c == 13)
+        {
+            break;
+        }
+        else if (c == 8) // Backspace key ascii value = 8
+        {
+            if (j > 0)
+            {
+                j--;
+                cout << "\b \b"; // Move the cursor back and erase the character
+            }
+        }
+        else if (c == 9 || c == 32)
+        {
+            continue;
+        }
+        else if (j < max_pin_len)
+        {
+            obj.pin[j] = c;
+            cout << "*";
+            j++;
+        }
+    }
+
+    obj.pin[j] = '\0';
+    ofstream fout1;
+    fout1.open("pin", ios::binary);
+    fout1.write((char *)&obj, sizeof(obj));
+    fout1.close();
+}
+int credentials ::login()
+{
+    credentials obj;
+    system("cls");
+    cout << "\t\t<<------Log In------>>" << endl;
+    cout << "\n\t\tUsername: ";
+    cin >> temp_un;
+    int i = 0;
+    cout << "\t\tPassword(10 characters): ";
+    while (true)
+    {
+        char ch = _getch();
+        if (ch == 13)
+        {
+            break;
+        }
+        else if (ch == 8) // Backspace key ascii value = 8
+        {
+            if (i > 0)
+            {
+                i--;
+                cout << "\b \b"; // Move the cursor back and erase the character
+            }
+        }
+        else if (ch == 9 || ch == 32)
+        {
+            continue;
+        }
+        else if (i < max_pass_len)
+        {
+            temp_pass[i] = ch;
+            cout << '*';
+            i++;
+        }
+    }
+    temp_pass[i] = '\0';
+    ifstream fin;
+    fin.open("p_confidentials", ios::binary);
+    fin.seekg(0, ios::beg);
+    fin.read((char *)&obj, sizeof(obj));
+    if (strcmp(obj.username, temp_un) == 0 && strcmp(obj.password, temp_pass) == 0)
+    {
+        cout << endl
+             << "\n\t\tLogin successful" << endl;
+        cout << "\t\tLogging in . . . ." << endl;
+        sleep(3);
+        return 1;
+    }
+    else
+    {
+        cout << endl
+             << "\n\t\tLogin failed! Invalid username or password." << endl;
+        cout << "\t\tPress any key to go back" << endl;
+        getch();
+        return 0;
+    }
+    fin.close();
+}
+void credentials ::forgot_pass()
+{
+    credentials obj;
+    system("cls");
+    int j = 0;
+    cout << "\n\t\tEnter the pin(4 characters):";
+    while (true)
+    {
+        char c = _getch();
+        if (c == 13)
+        {
+            break;
+        }
+        else if (c == 8) // Backspace key ascii value = 8
+        {
+            if (j > 0)
+            {
+                j--;
+                cout << "\b \b"; // Move the cursor back and erase the character
+            }
+        }
+        else if (c == 9 || c == 32) // 32 = ' ' and 9 = '\t'
+        {
+            continue;
+        }
+        else if (j < max_pin_len)
+        {
+            p[j] = c;
+            cout << '*';
+            j++;
+        }
+    }
+    p[j] = '\0';
+    ifstream fin;
+    fin.open("pin", ios ::binary);
+    fin.read((char *)&obj, sizeof(obj));
+    if (strcmp(p, obj.pin) == 0)
+    {
+        cout << endl;
+        signup();
+    }
+    else
+    {
+        cout << endl
+             << "\t\tWrong pin!!!!!" << endl;
+    }
+    cout << endl
+         << "\n\t\tPress any key to go back" << endl;
+    getch();
+    fin.close();
 }
 
 int main()
 {
-    char choice, x;
-    int loggedIn = 0;
-    student s;
-
-    while (1)
+    credentials obj;
+    ifstream fin;
+    fin.open("p_confidentials", ios::binary);
+    if (!fin.is_open())
     {
         system("cls");
-        cout << "\t\t\t\t\t<== STUDENT MANAGEMENT SYSTEM ==>\n";
-        cout << "\t\t*********************************************************************************\n";
-
-        if (!loggedIn)
+        cout << "\t\t<<------Sign up------>>" << endl;
+        obj.signup();
+        obj.pi_n();
+    }
+    fin.close();
+flag:
+    int loggedin = 0;
+    char ch;
+    while (loggedin == 0)
+    {
+        system("cls");
+        cout << "\n\t\ta.Login" << endl
+             << "\t\tb.Forgot password" << endl
+             << "\t\tc.Exit" << endl;
+        cout << "\n\t\tEnter your choice(a-c):";
+        cin >> ch;
+        switch (ch)
         {
-            cout << "\n\t\tLogin to continue:\n";
-            loggedIn = s.Login();
-            if (!loggedIn)
-            {
-                cout << "\n\t\tPress any key to retry...\n";
-                getch();
-                continue;
-            }
-        }
-
-        while (1)
+        case 'a':
         {
-            flag:
+            loggedin = obj.login();
+            break;
+        case 'b':
+            obj.forgot_pass();
+            break;
+        case 'c':
+            exit(0);
+        default:
             system("cls");
-            cout << "\t\t\t\t\t<== Student Management System ==>\n";
-            cout << "\t\t*********************************************************************************\n";
-            fflush(stdin);
-            cout << "\n\t\ta. Add Student\n";
-            cout << "\t\tb. Display All Students\n";
-            cout << "\t\tc. Search Student\n";
-            cout << "\t\td. Edit Student Details\n";
-            cout << "\t\te. Delete Student\n";
-            cout << "\t\tf. Exit\n";
-            cout << "\n\t\tEnter your choice(a-f): ";
-            cin >> choice;
-
-            switch (choice)
-            {
-            case 'a':
-                s.AddStudent();
-                break;
-
-            case 'b':
-                s.DisplayAllStudent();
-                break;
-
-            case 'c':
-                s.SearchStudent();
-                break;
-
-            case 'd':
-                s.EditStudentDetails();
-                break;
-
-            case 'e':
-                s.DeleteStudent();
-                break;
-
-            case 'f':
-                cout << "\t\tPress Y to exit and any other character to cancel: ";
-                fflush(stdin);
-                cin >> x;
-                if (x == 'Y' || x == 'y')
-                {
-                    exit(0);
-                }
-                else
-                {
-                    goto flag;
-                }
-                break;
-
-            default:
-                cout << "\n\t\tInvalid Choice!\n";
-                cout << "\t\tPlease Enter from a to f\n";
-                cout << "\t\tPress any key to retry";
-                getch();
-                goto flag;
-            }
-
-            cout << "\n\t\tPress any key to continue";
+            cout << "\t\tInvalid choice!!!!!" << endl;
+            cout << "\n\t\tPress any key to go to back" << endl;
             getch();
+            break;
+        }
+        }
+    }
+    while (1)
+    {
+        char choice;
+        student s;
+        system("cls");
+        cout << "\t\t\t\t\t<== Student Management System ==>\n";
+        cout << "\t\t*********************************************************************************\n";
+        fflush(stdin);
+        cout << "\n\t\ta. Add Student\n";
+        cout << "\t\tb. Display All Students\n";
+        cout << "\t\tc. Search Student\n";
+        cout << "\t\td. Edit Student Details\n";
+        cout << "\t\te. Delete Student\n";
+        cout << "\t\tf. Log Out\n";
+        cout << "\n\t\tEnter your choice(a-f): ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 'a':
+            s.AddStudent();
+            break;
+
+        case 'b':
+            s.DisplayAllStudent();
+            break;
+
+        case 'c':
+            s.SearchStudent();
+            break;
+
+        case 'd':
+            s.EditStudentDetails();
+            break;
+
+        case 'e':
+            s.DeleteStudent();
+            break;
+
+        case 'f':
+            system("cls");
+            exit(0);
+        default:
+            system("cls");
+            cout << "\t\tInvalid Choice...." << endl;
+            cout << "\t\tPlease Enter from a to d" << endl;
+            cout << "\n\t\tPress any key to go to menu....." << endl;
+            getch();
+            break;
         }
     }
 
     return 0;
 }
-
